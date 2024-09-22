acd1c52db09598d28de2637da8e98b49adda9a64
ontemfoiquarta <joaomgdcorreiacaldeira@gmail.com>
2024-09-22 14:18:05 +0100

    Updated learning.txt & script.js

--- JS FOUNDATIONS/script.js
+++ JS FOUNDATIONS/script.js
@@ -1,8 +1,22 @@
-let x = 4;
-let y = 5;
-x = y;
+let num1 = 1;
+let num2 = 3;
+let num3 = 12;
+let num4 = 24;
 
-x+=4;
-x-=3;
+let sum = num1 + num2;
+let subtract = num4 - num3;
+let finalResult = subtract * sum;
+let evenOddResult;
 
-console.log(x)
+
+
+if (finalResult % 2 === 0) {
+    evenOddResult = 0
+}
+else {
+    evenOddResult = 1
+}
+
+
+console.log (finalResult)
+console.log(evenOddResult)
