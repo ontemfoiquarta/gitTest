fcb1d0f9cb23742019eb75722d6a9954491fe5f0
ontemfoiquarta <joaomgdcorreiacaldeira@gmail.com>
2024-09-27 08:02:44 +0100

    Updated Learning.txt

--- JS FOUNDATIONS/script.js
+++ JS FOUNDATIONS/script.js
@@ -1,13 +1,3 @@
-let browser = prompt("What's your favorite browser?");
-let input = browser.toLowerCase()
+let name = "João"
 
-if(input == "edge") {
-    alert ("You've got the edge!")
-} else if (browser == "chrome"
-    || input == "firefox"
-    || input == "safari"
-    || input == "opera") {
-    alert ("Okay, we support those too")
-} else {
-    alert ("We hope that this page looks ok")
-}
+console.log(`Hello, ${3+4}`)
